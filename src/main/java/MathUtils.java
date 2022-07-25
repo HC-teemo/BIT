ce82f36114a38c684eadc759489816138bf8a583
胡川, Hu Chuan <2585814664@qq.com>
2022-07-25 13:35:11 +0800

    impl algos

--- src/main/java/MathUtils.java
+++ src/main/java/MathUtils.java
@@ -1,2 +1,68 @@
-package PACKAGE_NAME;public class MathFuncs {
+import scala.Array;
+
+import java.util.ArrayList;
+
+public class MathUtils {
+    public static final int CHAT_LIST_SIZE = 20; // C(20, 10) = 184756
+    public static final int COM_LIST_SIZE = 15; // C(15, 7) = 6453
+    public static int[] cList = new int[CHAT_LIST_SIZE];
+    public static int[][][] comList = new int[COM_LIST_SIZE][][];
+
+    static {
+        for (int i = 0; i < CHAT_LIST_SIZE; i++) cList[i] = c(i + 1, Math.floorDiv(i + 1, 2));
+        for (int i = 0; i < COM_LIST_SIZE; i++) {
+            comList[i] = combinations(i + 1, Math.floorDiv(i + 1, 2), 1).toArray(new int[][]{});
+        }
+    }
+
+    /*
+        r = ⌊n / 2⌋
+        c(n) = n! / r!(n-r)!
+     */
+    public static int c(int n, int r) {
+        long nu = 1;
+        long de = 1;
+        for (int i = r + 1; i <= n; i++) nu *= i;
+        for (int i = 2; i <= n - r; i++) de *= i;
+        return (int) (nu / de);
+    }
+
+    public static int cHat(int n) {
+        int k = 0;
+        while (k < CHAT_LIST_SIZE && cList[k] < n) k++;
+        return k + 1;
+    }
+
+    /**
+     *
+     * @param n bit number
+     * @return combinations of the bit
+     */
+    public static int[][] getCombinations(int n) {
+        return n <= COM_LIST_SIZE ?
+                comList[n-1] :
+                combinations(n, Math.floorDiv(n, 2), 1).toArray(new int[][]{});
+    }
+
+    private static ArrayList<int[]> combinations(int n, int k, int s) {
+        if (k > n || s > n) return new ArrayList<>();
+        ArrayList<int[]> coms = new ArrayList<>(c(n-s, k));
+        if (k == 1) {
+            for (int i = s; i <= n; i++) {
+                coms.add(new int[]{i});
+            }
+        } else {
+            ArrayList<int[]> sub1 = combinations(n, k - 1, s + 1);
+            sub1.forEach(i->{
+                int[] c = new int[k];
+                c[0] = s;
+                Array.copy(i, 0, c, 1, k-1);
+                coms.add(c);
+            });
+            ArrayList<int[]> sub0 = combinations(n, k, s + 1);
+            coms.addAll(sub0);
+        }
+        return coms;
+    }
+
 }
--- src/test/java/MathUtilsTest.java
+++ src/test/java/MathUtilsTest.java
@@ -1,34 +1,59 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-class MainTest {
+import java.util.ArrayList;
+import java.util.BitSet;
+
+class MathUtilsTest {
 
     @Test
     void c() {
-        Assertions.assertEquals(1, Main.c(1));
-        Assertions.assertEquals(2, Main.c(2));
-        Assertions.assertEquals(3, Main.c(3));
-        Assertions.assertEquals(6, Main.c(4));
-        Assertions.assertEquals(10, Main.c(5));
-        Assertions.assertEquals(20, Main.c(6));
-        Assertions.assertEquals(35, Main.c(7));
-        Assertions.assertEquals(70, Main.c(8));
-        Assertions.assertEquals(126, Main.c(9));
-        Assertions.assertEquals(252, Main.c(10));
+        Assertions.assertEquals(1, MathUtils.c(1, 0));
+        Assertions.assertEquals(2, MathUtils.c(2, 1));
+        Assertions.assertEquals(3, MathUtils.c(3, 1));
+        Assertions.assertEquals(6, MathUtils.c(4, 2));
+        Assertions.assertEquals(10, MathUtils.c(5, 2));
+        Assertions.assertEquals(20, MathUtils.c(6, 3));
+        Assertions.assertEquals(35, MathUtils.c(7, 3));
+        Assertions.assertEquals(70, MathUtils.c(8, 4));
+        Assertions.assertEquals(126, MathUtils.c(9, 4));
+        Assertions.assertEquals(252, MathUtils.c(10, 5));
     }
 
     @Test
     void CHat() {
-        Assertions.assertEquals(2, Main.cHat(2));
-        Assertions.assertEquals(3, Main.cHat(3));
-        Assertions.assertEquals(4, Main.cHat(4));
-        Assertions.assertEquals(4, Main.cHat(5));
-        Assertions.assertEquals(4, Main.cHat(6));
-        Assertions.assertEquals(5, Main.cHat(7));
-        Assertions.assertEquals(5, Main.cHat(9));
-        Assertions.assertEquals(5, Main.cHat(10));
-        Assertions.assertEquals(6, Main.cHat(11));
-        Assertions.assertEquals(6, Main.cHat(20));
-        Assertions.assertEquals(11, Main.cHat(253));
+        Assertions.assertEquals(2, MathUtils.cHat(2));
+        Assertions.assertEquals(3, MathUtils.cHat(3));
+        Assertions.assertEquals(4, MathUtils.cHat(4));
+        Assertions.assertEquals(4, MathUtils.cHat(5));
+        Assertions.assertEquals(4, MathUtils.cHat(6));
+        Assertions.assertEquals(5, MathUtils.cHat(7));
+        Assertions.assertEquals(5, MathUtils.cHat(9));
+        Assertions.assertEquals(5, MathUtils.cHat(10));
+        Assertions.assertEquals(6, MathUtils.cHat(11));
+        Assertions.assertEquals(6, MathUtils.cHat(20));
+        Assertions.assertEquals(11, MathUtils.cHat(253));
+    }
+
+    @Test
+    void getCombinations() {
+        Assertions.assertEquals(2, MathUtils.getCombinations(2).length);
+        Assertions.assertEquals(3, MathUtils.getCombinations(3).length);
+        Assertions.assertEquals(6, MathUtils.getCombinations(4).length);
+        Assertions.assertEquals(10, MathUtils.getCombinations(5).length);
+        Assertions.assertEquals(20, MathUtils.getCombinations(6).length);
+        Assertions.assertEquals(35, MathUtils.getCombinations(7).length);
+        Assertions.assertEquals(70, MathUtils.getCombinations(8).length);
+        Assertions.assertEquals(126, MathUtils.getCombinations(9).length);
+        Assertions.assertEquals(252, MathUtils.getCombinations(10).length);
+    }
+
+    @Test
+    void bitSetTest() {
+        BitSet bitSet = new BitSet();
+        bitSet = BitSet.valueOf(new long[]{0L});
+        System.out.println(bitSet);
+        bitSet = BitSet.valueOf(new long[]{1L});
+        System.out.println(bitSet);
     }
 }
